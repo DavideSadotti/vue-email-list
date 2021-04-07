0653bbbf2c243e809ae2a3b6ea9d150d48d1292c
Davide <d.sadotti@gmail.com>
2021-04-07 17:05:03 +0200

    Esercizio Finito (no css)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 var app = new Vue(
   {
     el: "#app",
-    data: {},
+    data: {
+      emailList: [],
+    },
     mounted: function(){
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((risposta1) => {
-        }
-      );
+      for(var i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((risposta) => {
+            this.emailList.push(risposta.data.response);
+          }
+        );
+      }
     }
   }
 );
